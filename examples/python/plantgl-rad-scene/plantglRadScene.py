e31b9b5c4fe31605c95c119e099a664ad017144b
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2024-12-02 10:52:07 +0100

    removing unneeded results files + rename example

--- examples/python/plantgl-rad-scene/plantglRadScene.py
+++ examples/python/plantgl-rad-scene/plantglRadScene.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     simulator.addEnvFromFile("./assets/testChamber.rad", "./PO")
     simulator.addVirtualDiskSensorsFromFile("./captors/captors_expe1.csv")
 
-    simulator.setupRender(Vec3(68.0, 1200.0, 1500.0), Vec3(1280.0, 860.0, 980.0), 75.0)
-    simulator.n_samples = 2
+    # simulator.setupRender(Vec3(68.0, 1200.0, 1500.0), Vec3(1280.0, 860.0, 980.0), 75.0)
+    # simulator.n_samples = 2
     res = simulator.run()
 
-    simulator.visualizeScene()
+    simulator.visualizeResults()
 
 # command visualiser Environnement PlantGL
 # ipython
